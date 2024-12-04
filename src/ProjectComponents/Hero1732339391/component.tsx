9a1ae9307408b4f4b4e9ff35d47fa755bf72c481
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-12-04 21:10:34 +0000

    feat: Edit Hero1732339391
    Requested change: remove all images

--- src/ProjectComponents/Hero1732339391/component.tsx
+++ src/ProjectComponents/Hero1732339391/component.tsx
@@ -5,12 +5,9 @@ const Hero: React.FC = () => {
   return (
     <div className="bg-black py-16 text-white w-full h-full">
       <div className="container mx-auto px-4 flex flex-col md:flex-row items-center h-full">
-        <div className="md:w-1/2 mb-8 md:mb-0">
+        <div className="md:w-full">
           <h1 className="text-4xl font-bold mb-4">Unleash Your Inner Paleontologist</h1>
           <p className="text-xl mb-6">Buy, sell, and trade preaaaaahistoric wonders in the world's premier dinosaur marketplace</p>
-        </div>
-        <div className="md:w-1/2">
-          <img src={`https://raw.githubusercontent.com/56b81caaa87941618cfed6dfb4d34047/Dinosaur_Trading_Platform_1732339388/${window.MI_PROJECT_GIT_REF || 'main'}/src/assets/images/e06abc68a2994f44959707af090900a9.jpeg`} alt="Rock" className="w-full h-auto rounded-lg shadow-md" />
         </div>
       </div>
     </div>
